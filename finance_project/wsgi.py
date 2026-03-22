3edd3b071ab227330949930fb7950e5d86a4b0bc
Mafiz Uddin Chowdhury <mafizchowdhury006@gmail.com>
2026-03-22 22:43:04 +0600

    Refactor WSGI file to remove pip dependencies
    Removed pip installation calls and adjusted imports.

--- finance_project/wsgi.py
+++ finance_project/wsgi.py
@@ -9,14 +9,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 import sys
-import pip
+
 
 from django.core.wsgi import get_wsgi_application
-pip.main(['install','setuptools'])
-try:
-  import pkg_rsources
-except importError:
-  pip.main(['install','setuptools'])
+
   import pkg_resources
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance_project.settings')
 try:
